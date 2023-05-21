8765c620bccf168921c92a32e4a8a4460acba351
Brooks Jackson <90881109+bjaxqq@users.noreply.github.com>
2023-05-20 23:41:04 -0400

    Create Question11.java

---
+++ Homework/Week 5 Assignment/Question11.java
@@ -0,0 +1,32 @@
+public class Question11 {
+    private String name;
+    private int age;
+
+    // Constructor to create pets with a name
+    public Question11(String name) {
+        this.name = name;
+    }
+
+    // Constructor to create pets with a name and an age
+    public void Quesiton11(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters and setters (optional)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
